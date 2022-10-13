042f4dc01ebe89ec6e0871320c66cc28610f707f
rony derra <derrarony@gmail.com>
2022-10-13 12:42:41 +0300

    contract buisness logic

---
+++ src/buisiness-logic/contract-logic.ts
@@ -0,0 +1,33 @@
+import { Contract } from "../models/contract";
+
+export const getContract = async () => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const postContract = async () => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const updateContract = async () => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const deleteContract = async () => {
+    try {
+
+    } catch (error) {
+
+    }
+}
