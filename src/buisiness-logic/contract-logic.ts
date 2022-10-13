920bb03d9651fc53180e2d8e847636be160a8b8c
rony derra <derrarony@gmail.com>
2022-10-13 14:25:06 +0300

    logic - post contract

--- src/buisiness-logic/contract-logic.ts
+++ src/buisiness-logic/contract-logic.ts
@@ -1,6 +1,6 @@
 import { Contract } from "../models/contract";
 
-export const getContract = async () => {
+export const getContract = async (data) => {
     try {
 
     } catch (error) {
@@ -8,11 +8,39 @@ export const getContract = async () => {
     }
 }
 
-export const postContract = async () => {
+export const postContract = async (data) => {
     try {
+        const {
+            clientAddress, destinationCahin, contractStandart,
+            collectionName, tokenTicker, royalties, royaltiesPercentage,
+            royaltiesAddress, targetMarketplace,
+            chain, contractAddress, keepOriginalTokenIds,
+            transferOnlyWithIds, ids, ownershipTransferd
+        } = data
 
+        const res = await Contract.create({
+            clientAddress,
+            destinationCahin,
+            contractStandart,
+            collectionName,
+            tokenTicker,
+            royalties,
+            royaltiesPercentage,
+            royaltiesAddress,
+            targetMarketplace,
+            departureDetailes: [{
+                chain,
+                contractAddress,
+                keepOriginalTokenIds,//bool
+                transferOnlyWithIds,//bool
+                ids
+            }],
+            ownershipTransferd
+        })
+        return res;
     } catch (error) {
-
+        console.log(error);
+        return undefined;
     }
 }
 
